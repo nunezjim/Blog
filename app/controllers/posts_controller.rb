fa67dba5c8d6a5fecc110645d0863bd5dd35a05e
Jimmy Nunez <jimmyjnunez@gmail.com>
2016-09-13 11:36:34 -0400

    implemented the CRUD method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,40 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @posts = Post.order("updated_at DESC") #to show the most recent updated blog
   end
 
  def new
    @post = Post.new
  end
 
-#   def create
-#   @post = Post.new()
-# end
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path
+    end
+  end
 
+  def edit
+    @post = Post.where(id: params[:id]).first
+  end
+
+  def update
+    @post = Post.where(id: params[:id]).first
+    if @post.update_attributes(post_params)
+      redirect_to posts_path
+    end
+  end
+
+  def destroy
+    @post = Post.where(id: params[:id]).first #.first is going to load the post with the ID you are clicking
+    if @post.destroy
+      redirect_to posts_path
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
 end
