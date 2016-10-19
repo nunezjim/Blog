a70848b40f5800a6990d881e6daa8f8c896d49b1
Jimmy Nunez <jimmyjnunez@gmail.com>
2016-10-19 12:48:46 -0400

    Added the context of a user to a comment by generating a migration file to add user_id attribute to the comments mode;

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,12 +2,14 @@ class CommentsController < ApplicationController
   def create
     @post = Post.where(id: params[:post_id]).first
     @comment = @post.comments.build(comment_params)
+    @comment.user = current_user
     if @comment.save
       redirect_to post_path(@post)
-  end
+    else
+      #error message
     end
-
-   private
+  end
+private
 
    def comment_params
      params.require(:comment).permit(:body)
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :post
-end
+  belongs_to :user
+
+  validates_presence_of :user  #if you want an anonymous to comment. You need to comment this line.
+end 
